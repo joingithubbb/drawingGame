0a3e34f82fecfd76c8b8e5dc49c88a1d7137e6de
53091601 <florian.haenny@icr.ch>
2019-09-20 11:54:08 +0200

    initial commit

---
+++ build/precache-manifest.bd9a4b032b1f003c1eab3783a16565c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3877a52f1e943b3d329",
+    "url": "/react-course-project/static/js/runtime~main.c3877a52.js"
+  },
+  {
+    "revision": "1e5b122ab311b9bd1f24",
+    "url": "/react-course-project/static/js/main.1e5b122a.chunk.js"
+  },
+  {
+    "revision": "ef3806447dab1d19d303",
+    "url": "/react-course-project/static/js/1.ef380644.chunk.js"
+  },
+  {
+    "revision": "1e5b122ab311b9bd1f24",
+    "url": "/react-course-project/static/css/main.9fee4d9c.chunk.css"
+  },
+  {
+    "revision": "78a0391482aa70c72f9fe5f3790cf6c9",
+    "url": "/react-course-project/index.html"
+  }
+];
