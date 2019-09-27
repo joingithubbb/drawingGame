7cdba4efc4b63539334001530e4770f6b62370e0
53091601 <florian.haenny@icr.ch>
2019-09-27 13:02:57 +0200

    files added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import GameEngine from "./GameEngine";
@@ -30,10 +31,34 @@ function EndRouter() {
     <div>
       <h2>End</h2>
       <button type="submit" onClick={routeToHome}>Home</button>
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> Initial commit from Create React App
     </div>
   );
 }
 
+<<<<<<< HEAD
 function AppRouter() {
   return (
     <Router>
@@ -48,3 +73,6 @@ function AppRouter() {
 
 export default AppRouter;
 export { App };
+=======
+export default App;
+>>>>>>> Initial commit from Create React App
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -18,3 +19,17 @@ serviceWorker.unregister();
 
 /*     <Canvas ref={ref} />
     <Controls theCanvas={ref} model={model} labels={labels} /> */
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+>>>>>>> Initial commit from Create React App
