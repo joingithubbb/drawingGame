7aef38bb034dd1f7b83d3a99bce62847c3ba9c8e
53091601 <florian.haenny@icr.ch>
2019-09-27 14:37:16 +0200

    First router implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { GameEngine } from "./GameEngine";
 
 
 function App() {
-
-  return <GameEngine />;
+  return (
+  <div>
+    <AppRouter />
+    <GameEngine />
+  </div>
+  );
 }
 
+function HomeRouter() {
+  return <h2>Home</h2>;
+}
 
+function GameRouter() {
+  return <h2>Game</h2>;
+}
 
-export { App };
+function EndRouter() {
+  
+  function routeToHome() {
+
+    window.location.replace('./');
+  };
+
+  return (
+    <div>
+      <h2>End</h2>
+      <button type="submit" onClick={routeToHome}>Home</button>
+    </div>
+  );
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <Route path="/" exact component={HomeRouter} />
+        <Route path="/game" component={GameRouter} />
+        <Route path="/end" component={EndRouter} />
+      </div>
+    </Router>
+  );
+}
+
+export { App, AppRouter };
