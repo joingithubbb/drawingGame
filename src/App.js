c5c3685c270c767dea31dc447ea6369264f20dc3
53091601 <florian.haenny@icr.ch>
2019-09-27 17:55:40 +0200

    Moved my routercomponent and logic to another file.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Switch } from "react-router";
 import { GameEngine } from "./GameEngine";
-
 
 function App() {
   return (
   <div>
-    <AppRouter />
     <GameEngine />
   </div>
   );
 }
 
-function HomeRouter() {
-  return <h2>Home</h2>;
-}
 
-function GameRouter() {
-  return <h2>Game</h2>;
-}
 
-function EndRouter() {
-  
-  function routeToHome() {
-
-    window.location.replace('./');
-  };
-
-  return (
-    <div>
-      <h2>End</h2>
-      <button type="submit" onClick={routeToHome}>Home</button>
-    </div>
-  );
-}
-
-function NoMatch() {
-
-  return <h1>Nothing to see here - Four Oh! Four</h1>
-}
-
-function AppRouter() {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/" exact component={HomeRouter} />
-          <Route path="/game" component={GameRouter} />
-          <Route path="/end" component={EndRouter} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-export { App, AppRouter };
+export { App };
--- src/GameEngine.js
+++ src/GameEngine.js
@@ -1,11 +1,66 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router";
 import { Game } from "./Game";
+import { Home } from "./Home";
+import { End } from "./End";
 
 function GameEngine() {
 
     return(
-        <Game />
+        <AppRouter />
     )
 }
+
+
+//-----Router-Stuff---------
+
+function HomeRouter() {
+    return <h2>Home</h2>;
+  }
+  
+  function GameRouter() {
+    return(
+      <div>
+        <h2>Game</h2>
+        <Game />
+      </div>
+    )
+  }
+  
+  function EndRouter() {
+    
+    function routeToHome() {
+  
+      window.location.replace('./');
+    };
+  
+    return (
+      <div>
+        <h2>End</h2>
+        <button type="submit" onClick={routeToHome}>Home</button>
+      </div>
+    );
+  }
+  
+  function NoMatch() {
+  
+    return <h1>Nothing to see here - Four Oh! Four</h1>
+  }
+  
+  function AppRouter() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact component={HomeRouter} />
+            <Route path="/game" component={GameRouter} />
+            <Route path="/end" component={EndRouter} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 
 export { GameEngine };
