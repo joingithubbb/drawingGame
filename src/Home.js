c79469531f16cb0236d83a4483ddae0b93174524
53091601 <florian.haenny@icr.ch>
2019-10-01 15:42:29 +0200

    Implemented "Home"

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { RouteToGame } from "./GameEngine";
 
  function Home() {
 
      return(
-        <h2>Home</h2>
+        <div>
+            <h1>Sketch</h1>
+            <InfoTextHome />
+            <PlayGameButton />
+        </div>
      )
  }
 
- export { Home };
+ function InfoTextHome() {
+
+    return (
+        <div>
+            <p1>Welcome to Sketch the Drawing Game!</p1>
+            <br></br>
+            <p2>This is a game inspired by Google's "Quick, Draw!" - You'll have to draw quickly!</p2>
+        </div>
+    )
+ }
+
+    function PlayGameButton() {
+        return (
+            <button type="submit" onClick={RouteToGame}>Play Game</button>
+        )
+    }
+
+ export { Home, PlayGameButton };
