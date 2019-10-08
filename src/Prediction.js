d3280a1b0217c49d52187bba789f556cb18f70d9
53091601 <florian.haenny@icr.ch>
2019-10-08 15:02:34 +0200

    First implemetation of the automatic prediction

--- src/Prediction.js
+++ src/Prediction.js
@@ -4,9 +4,29 @@ import { getPrediction } from "./helpers.js";
 function Prediction({ theCanvas, model, labels }) {
   let [prediction, setPrediction] = useState(""); // Sets default label to empty string.
 
+
   useEffect(() => {
     console.log(prediction);
   });
+
+  //Predict every second
+  useEffect(() => {
+    predict();
+  }, []);
+
+  function predict() {
+    setInterval(
+      function pred() {
+        getPrediction(theCanvas, model).then(prediction =>
+          setPrediction(labels[prediction[0]])
+        )
+      }
+      , 1000
+    );
+  }
+
+
+
 
   return (
     <div>
