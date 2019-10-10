f1f768ecfebfa917182a669292bd73b70dd6190c
53091601 <florian.haenny@icr.ch>
2019-10-10 12:22:25 +0200

    Start of automatic prediction on button click

--- src/Game.js
+++ src/Game.js
@@ -1,28 +1,59 @@
+import { useState } from "react";
 import { Canvas } from "./Canvas";
 import { Controls } from "./Controls";
 import { Prediction } from "./Prediction";
+import { RouteToGame } from "./GameEngine"
 
 import React from "react";
 import "./index.css";
 import * as tf from "@tensorflow/tfjs";
 
+function Countdown() {
+    console.log("Hello from Countdown");
+    return (
+        <div>
+            <h1>TESTESTEST</h1>
+        </div>
+    );
+}
 
-
-function Game(){
+function Game() {
+    const [gameOn, setGameOn] = useState(false);
 
     const model = tf.loadModel("./model/model.json");
     const labels = require("./labels.json");
     let ref = React.createRef();
 
-    return(
-    <div>
-        <h3>You'll have to draw the requested thing!</h3>
-        <h4>For each correct answer you'll get a point. Five points to win! But beware of the countdown!</h4>
-        <Canvas ref={ref} />
-        <Controls theCanvas={ref} />
-        <Prediction theCanvas={ref} model={model} labels={labels} />
-    </div>
-        )
+
+
+    if (!gameOn) {
+    return (
+        <div>
+            { console.log("Hello from !gameOn") }
+            <h3>You'll have to draw the requested thing!</h3>
+            <h4>For each correct answer you'll get a point. Five points to win! But beware of the countdown!</h4>
+            <button type="submit" onClick={() => setGameOn(true)}>Start Game</button>
+            <div>
+                <Canvas ref={ref} />
+                <Controls theCanvas={ref} />
+            </div>
+        </div>
+    )}
+
+    else {
+    return (
+        <div>
+            { console.log("Hello from gameOn") }
+            <h3>You'll have to draw the requested thing!</h3>
+            <h4>For each correct answer you'll get a point. Five points to win! But beware of the countdown!</h4>
+            <button type="submit" onClick={RouteToGame}>Reset Game</button>
+            <div>
+                <Canvas ref={ref} />
+                <Controls theCanvas={ref} />
+                <Prediction theCanvas={ref} model={model} labels={labels} />
+            </div>
+        </div>
+    )}
 };
 
 export { Game };
