ff7dc4169e48d5ed594223b9b406a5ac6bb85dda
53091601 <florian.haenny@icr.ch>
2019-12-20 11:50:55 +0100

    finished the round test

--- src/Round.test.js
+++ src/Round.test.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Round } from './Round';
+import { App } from './App';
 import Enzyme from 'enzyme';
+import { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 Enzyme.configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Round />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it("receives a number", () => {
+    const props = {
+        round: 5
+    },
+        RoundComponent = mount(<Round {...props} />).find(Round);
+
+    expect(typeof RoundComponent.props().round).toBe('number');
+
 });
