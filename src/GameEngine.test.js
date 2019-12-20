e123669378c1fcc67ff015ddaffa4d0e4e3a2506
53091601 <florian.haenny@icr.ch>
2019-12-20 15:53:24 +0100

    Added new test

---
+++ src/GameEngine.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GameEngine } from './GameEngine';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from "react-test-renderer";
+Enzyme.configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<GameEngine />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+//For checking after a refactor
+it("should render <App />", () => {
+    const componentTree = renderer.create(<GameEngine />).toJSON();
+    expect(componentTree).toMatchSnapshot();
+  });
